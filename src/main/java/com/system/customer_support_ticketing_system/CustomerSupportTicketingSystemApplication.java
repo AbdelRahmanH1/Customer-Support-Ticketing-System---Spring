62212d07add532df1abb5d202b48e131ea6db77d
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-20 18:06:03 +0300

    Project setup

---
+++ src/main/java/com/system/customer_support_ticketing_system/CustomerSupportTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.customer_support_ticketing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSupportTicketingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerSupportTicketingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/system/customer_support_ticketing_system/CustomerSupportTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.customer_support_ticketing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSupportTicketingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
