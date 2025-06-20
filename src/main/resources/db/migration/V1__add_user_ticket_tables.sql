e7bf44727b13603d5dada976679b6081c1f65e30
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-20 21:58:43 +0300

    Add Users and Tickets mirgration file

---
+++ src/main/resources/db/migration/V1__add_user_ticket_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE users (
+                       id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                       name VARCHAR(100),
+                       email VARCHAR(100) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       role VARCHAR(20) NOT NULL,
+                       deleted BOOLEAN DEFAULT FALSE,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tickets (
+                         id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                         title VARCHAR(255),
+                         description TEXT,
+                         status VARCHAR(20),
+                         user_id BIGINT,
+                         admin_responder_id BIGINT,
+                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                         end_time TIMESTAMP,
+
+                         FOREIGN KEY (user_id) REFERENCES users(id),
+                         FOREIGN KEY (admin_responder_id) REFERENCES users(id)
+);
