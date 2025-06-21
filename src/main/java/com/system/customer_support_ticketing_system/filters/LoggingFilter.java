087e16c7ff08d35b16e90fbd2c039204c5b646b9
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-21 19:42:39 +0300

    Add Logging Filter

--- src/main/java/com/system/customer_support_ticketing_system/filters/LoggingFilter.java
+++ src/main/java/com/system/customer_support_ticketing_system/filters/LoggingFilter.java
@@ -1,4 +1,24 @@
 package com.system.customer_support_ticketing_system.filters;
 
-public class LoggingFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Configuration
+public class LoggingFilter extends OncePerRequestFilter {
+    private static final Logger log = LoggerFactory.getLogger(LoggingFilter.class);
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("Request {}", request.getRequestURI());
+        filterChain.doFilter(request, response);
+        log.info("Response {}", response.getStatus());
+
+    }
 }
