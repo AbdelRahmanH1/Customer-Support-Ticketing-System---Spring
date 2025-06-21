72cf967298fe46486768fbb3aa0a5a4209080b12
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-21 19:47:48 +0300

    Add Global Exception  && AuthenticationEntryPoint

--- src/main/java/com/system/customer_support_ticketing_system/exceptions/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/system/customer_support_ticketing_system/exceptions/CustomAuthenticationEntryPoint.java
@@ -1,4 +1,26 @@
 package com.system.customer_support_ticketing_system.exceptions;
 
-public class CustomAuthenticationEntryPoint {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        Map<String, Object> map = new HashMap<>();
+        map.put("success",false);
+        map.put("message","Token Not valid Or not found");
+        String json = objectMapper.writeValueAsString(map);
+        response.getWriter().write(json);
+    }
 }
--- src/main/java/com/system/customer_support_ticketing_system/exceptions/GlobalException.java
+++ src/main/java/com/system/customer_support_ticketing_system/exceptions/GlobalException.java
@@ -1,4 +1,34 @@
 package com.system.customer_support_ticketing_system.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestControllerAdvice
 public class GlobalException {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        return  buildResponse("Page not found",HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpClientErrorException.Forbidden.class)
+    public ResponseEntity<Map<String, Object>> handleForbiddenException(NoHandlerFoundException e) {
+        return  buildResponse("No access",HttpStatus.FORBIDDEN);
+    }
+
+    private ResponseEntity<Map<String, Object>> buildResponse(String message,HttpStatus status) {
+        Map<String,Object> body = new HashMap<>();
+        body.put("success",false);
+        body.put("message",message);
+        return  ResponseEntity.status(status).body(body);
+    }
+
 }
