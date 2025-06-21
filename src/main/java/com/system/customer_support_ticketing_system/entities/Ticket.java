36f1f4c67fbfd7850b6182c531102b8dfc287718
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-21 19:41:25 +0300

    Update long id to Long id

--- src/main/java/com/system/customer_support_ticketing_system/entities/Ticket.java
+++ src/main/java/com/system/customer_support_ticketing_system/entities/Ticket.java
@@ -15,7 +15,7 @@ public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private long id;
+    private Long id;
 
     @Column(name = "title")
     private String title;
