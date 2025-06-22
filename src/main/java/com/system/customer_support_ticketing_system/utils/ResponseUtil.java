5ecc23f01de4de3225d077910547bef1af45cb71
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-22 19:58:27 +0300

    Add response utils for success

--- src/main/java/com/system/customer_support_ticketing_system/utils/ResponseUtil.java
+++ src/main/java/com/system/customer_support_ticketing_system/utils/ResponseUtil.java
@@ -1,4 +1,22 @@
 package com.system.customer_support_ticketing_system.utils;
 
+import com.system.customer_support_ticketing_system.dtos.BaseResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class ResponseUtil {
+
+    public static <T> ResponseEntity<BaseResponse<T>> success(T data){
+        return ResponseEntity.ok(new BaseResponse<>(true,null,data));
+    }
+
+    public static <T> ResponseEntity<BaseResponse<T>> success(String message,T data){
+        return ResponseEntity.ok(new BaseResponse<>(true,message,data));
+    }
+
+    public static <T> ResponseEntity<BaseResponse<T>> success(String message, T data, HttpStatus status){
+        return ResponseEntity.status(status).body(new BaseResponse<>(true,message,data));
+    }
+
+
 }
