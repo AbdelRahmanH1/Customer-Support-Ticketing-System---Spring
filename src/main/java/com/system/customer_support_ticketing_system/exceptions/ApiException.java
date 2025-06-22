e789b55d57dc9ec8377d70faf29249ffafd8be84
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-22 20:20:30 +0300

    Add apiException class

--- src/main/java/com/system/customer_support_ticketing_system/exceptions/ApiException.java
+++ src/main/java/com/system/customer_support_ticketing_system/exceptions/ApiException.java
@@ -1,4 +1,13 @@
 package com.system.customer_support_ticketing_system.exceptions;
 
-public class ApiException {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiException extends RuntimeException{
+    private final HttpStatus status;
+    public ApiException(String message, HttpStatus status){
+        super(message);
+        this.status = status;
+    }
 }
