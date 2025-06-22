9f4828bea34f8a343a9f7d6ba838a7307b52d273
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-22 21:03:02 +0300

    Add toDo mapper

--- src/main/java/com/system/customer_support_ticketing_system/mappers/UserMapper.java
+++ src/main/java/com/system/customer_support_ticketing_system/mappers/UserMapper.java
@@ -1,10 +1,12 @@
 package com.system.customer_support_ticketing_system.mappers;
 
 import com.system.customer_support_ticketing_system.dtos.CreateUserDto;
+import com.system.customer_support_ticketing_system.dtos.UserResponse;
 import com.system.customer_support_ticketing_system.entities.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    UserResponse toDto(User user);
     User toEntity(CreateUserDto request);
 }
