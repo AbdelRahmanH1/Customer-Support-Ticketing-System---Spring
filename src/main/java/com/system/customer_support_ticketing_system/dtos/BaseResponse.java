8fbce098d09f17a5d5d455fecd81201e9f4b74cc
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-22 19:57:10 +0300

    Add BaseResponse for success

--- src/main/java/com/system/customer_support_ticketing_system/dtos/BaseResponse.java
+++ src/main/java/com/system/customer_support_ticketing_system/dtos/BaseResponse.java
@@ -1,4 +1,12 @@
 package com.system.customer_support_ticketing_system.dtos;
 
-public class BaseResponse {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BaseResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
 }
