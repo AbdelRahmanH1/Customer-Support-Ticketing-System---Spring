224bc643033bf2f5cd3697870f1ce1771bdaa3f6
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-22 20:19:37 +0300

    Add userMapper interface

--- src/main/java/com/system/customer_support_ticketing_system/mappers/UserMapper.java
+++ src/main/java/com/system/customer_support_ticketing_system/mappers/UserMapper.java
@@ -1,4 +1,10 @@
 package com.system.customer_support_ticketing_system.mappers;
 
+import com.system.customer_support_ticketing_system.dtos.CreateUserDto;
+import com.system.customer_support_ticketing_system.entities.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+    User toEntity(CreateUserDto request);
 }
