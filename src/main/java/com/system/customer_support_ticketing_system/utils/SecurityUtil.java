812daac44d13e2a13c4fc750bbf4b862ac8f12a3
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-23 18:33:19 +0300

    Add getRole from Context

--- src/main/java/com/system/customer_support_ticketing_system/utils/SecurityUtil.java
+++ src/main/java/com/system/customer_support_ticketing_system/utils/SecurityUtil.java
@@ -6,4 +6,12 @@ public final class SecurityUtil {
     public static Long getUserId() {
         return (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
+
+    public static String getUserRole() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities()
+                .stream()
+                .findFirst()
+                .map(auth -> auth.getAuthority().replace("ROLE_", ""))
+                .orElse(null);
+    }
 }
