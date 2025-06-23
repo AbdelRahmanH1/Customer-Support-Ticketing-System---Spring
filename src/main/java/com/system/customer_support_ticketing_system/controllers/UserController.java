e1900ceacd7934b13d51afe78af4939b39b1f611
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-23 15:23:27 +0300

    Add getCurrentUser in service and controller

--- src/main/java/com/system/customer_support_ticketing_system/controllers/UserController.java
+++ src/main/java/com/system/customer_support_ticketing_system/controllers/UserController.java
@@ -1,4 +1,27 @@
 package com.system.customer_support_ticketing_system.controllers;
 
+import com.system.customer_support_ticketing_system.mappers.UserMapper;
+import com.system.customer_support_ticketing_system.repositories.UserRepository;
+import com.system.customer_support_ticketing_system.services.UserService;
+import com.system.customer_support_ticketing_system.utils.ResponseUtil;
+import com.system.customer_support_ticketing_system.utils.SecurityUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@AllArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getCurrentUser(){
+        var userId = SecurityUtil.getUserId();
+        var userDto = userService.getCurrentUser(userId);
+        return ResponseUtil.success("Fetched Data successfully",userDto);
+    }
 }
--- src/main/java/com/system/customer_support_ticketing_system/services/UserService.java
+++ src/main/java/com/system/customer_support_ticketing_system/services/UserService.java
@@ -1,4 +1,24 @@
 package com.system.customer_support_ticketing_system.services;
 
+import com.system.customer_support_ticketing_system.dtos.UserResponse;
+import com.system.customer_support_ticketing_system.mappers.UserMapper;
+import com.system.customer_support_ticketing_system.repositories.UserRepository;
+import com.system.customer_support_ticketing_system.utils.SecurityUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserResponse getCurrentUser(Long userId){
+        var user = userRepository.findById(userId).orElseThrow(
+                ()->new UsernameNotFoundException("User not found")
+        );
+        return userMapper.toDto(user);
+    }
 }
