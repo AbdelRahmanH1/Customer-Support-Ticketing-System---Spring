6a3da0042e2f57b78df7e323788c9030e7be7b3c
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-23 15:46:01 +0300

    Update chcek if the user is disable or not

--- src/main/java/com/system/customer_support_ticketing_system/filters/JwtFilter.java
+++ src/main/java/com/system/customer_support_ticketing_system/filters/JwtFilter.java
@@ -1,25 +1,32 @@
 package com.system.customer_support_ticketing_system.filters;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.system.customer_support_ticketing_system.auth.JwtService;
+import com.system.customer_support_ticketing_system.repositories.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @AllArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var authHeader = request.getHeader("Authorization");
@@ -34,6 +41,21 @@ public class JwtFilter extends OncePerRequestFilter {
         }
         var role = jwtService.getRoleFromToken(token);
         var userID = jwtService.getUserIdFromToken(token);
+
+        var user = userRepository.findById(userID).orElseThrow(() ->  new UsernameNotFoundException("User not found"));
+        if (user.isDeleted()) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+
+            Map<String, Object> errorBody = Map.of(
+                    "success", false,
+                    "message", "User account is deleted."
+            );
+
+            new ObjectMapper().writeValue(response.getOutputStream(), errorBody);
+            return;
+        }
+
 
         var authentication = new UsernamePasswordAuthenticationToken(
                 userID,
