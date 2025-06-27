525c97b4d091dc4c7e0bd4ce066edc06bb72a8f0
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-27 22:31:53 +0300

    Change the EmailCodeView to interface

--- src/main/java/com/system/customer_support_ticketing_system/dtos/EmailCodeView.java
+++ src/main/java/com/system/customer_support_ticketing_system/dtos/EmailCodeView.java
@@ -1,11 +1,7 @@
 package com.system.customer_support_ticketing_system.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
-@AllArgsConstructor
-public class EmailCodeView {
-    private String email;
-    private String passwordResetCode;
+public interface EmailCodeView {
+     String getEmail();
+     String getPasswordResetCode();
 }
