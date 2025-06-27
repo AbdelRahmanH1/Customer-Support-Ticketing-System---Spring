94a9afc133e2cab7757137357c6b43c86a242135
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-27 17:51:52 +0300

    Add SendMail service

--- src/main/java/com/system/customer_support_ticketing_system/services/EmailService.java
+++ src/main/java/com/system/customer_support_ticketing_system/services/EmailService.java
@@ -1,4 +1,29 @@
 package com.system.customer_support_ticketing_system.services;
 
+import com.system.customer_support_ticketing_system.exceptions.EmailSendingException;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            message.setFrom("your@gmail.com");
+
+            mailSender.send(message);
+        } catch (MailException e) {
+            throw new EmailSendingException("Failed to send email to " + to);
+        }
+    }
 }
