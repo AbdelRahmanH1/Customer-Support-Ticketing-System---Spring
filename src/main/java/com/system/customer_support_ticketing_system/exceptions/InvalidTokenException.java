8af5888c6cc82005777135266b936299e629623d
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-06-28 18:32:00 +0300

    Update: function accept message

--- src/main/java/com/system/customer_support_ticketing_system/exceptions/InvalidTokenException.java
+++ src/main/java/com/system/customer_support_ticketing_system/exceptions/InvalidTokenException.java
@@ -3,7 +3,7 @@ package com.system.customer_support_ticketing_system.exceptions;
 import org.springframework.http.HttpStatus;
 
 public class InvalidTokenException extends ApiException{
-    public InvalidTokenException() {
-        super("Invalid Token", HttpStatus.BAD_REQUEST);
+    public InvalidTokenException(String message) {
+        super(message, HttpStatus.BAD_REQUEST);
     }
 }
